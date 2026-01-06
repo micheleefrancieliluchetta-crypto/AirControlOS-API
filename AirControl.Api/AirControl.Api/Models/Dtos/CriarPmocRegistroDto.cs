5a39a9a42c091808b37985e003ffb04ca634db39
micheleefrancieliluchetta-crypto <micheleefrancieliluchetta@gmail.com>
2026-01-06 16:09:30 -0300

    Add files via upload

---
+++ AirControl.Api/AirControl.Api/Models/Dtos/CriarPmocRegistroDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AirControl.Api.Dtos
+{
+    public class CriarPmocRegistroDto
+    {
+        public int AparelhoHdvId { get; set; }
+
+        public DateTime Data { get; set; }
+
+        // mesmo nome do front e do modelo
+        public string ChecklistJson { get; set; } = string.Empty;
+
+        public string? ObservacoesTecnicas { get; set; }
+
+        public string? TecnicoNome { get; set; }
+    }
+}
