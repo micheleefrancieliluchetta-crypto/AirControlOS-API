72ce232aed9c1830966da3b880110022fc2714c0
micheleefrancieliluchetta-crypto <micheleefrancieliluchetta@gmail.com>
2026-01-13 10:49:36 -0300

    Update CriarPmocRegistroDto.cs

--- AirControl.Api/AirControl.Api/Models/Dtos/CriarPmocRegistroDto.cs
+++ AirControl.Api/AirControl.Api/Models/Dtos/CriarPmocRegistroDto.cs
@@ -1,20 +1,9 @@
-namespace AirControl.Api.Dtos
+public class CriarPmocRegistroDto
 {
-    public class CriarPmocRegistroDto
-    {
-        public int AparelhoHdvId { get; set; }
-
-        // Data vem como string do front (ou vazia). Vamos tratar no controller.
-        public string? Data { get; set; }
-
-        // JSON com os itens do checklist
-        public string ChecklistJson { get; set; } = "[]";
-
-        // Observações técnicas gerais (textarea no fim do PMOC)
-        public string? ObservacoesTecnicas { get; set; }
-
-        // Dados do técnico (opcionais)
-        public string? TecnicoNome { get; set; }
-        public string? TecnicoEmail { get; set; }
-    }
+    public int AparelhoHdvId { get; set; }
+    public string Data { get; set; }  // <- deve ser string
+    public string ChecklistJson { get; set; }
+    public string ObservacoesTecnicas { get; set; }
+    public string TecnicoNome { get; set; }
+    public string TecnicoEmail { get; set; }
 }
